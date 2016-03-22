21a67af483107632f25cd779999da885b032b20c
Ben <0912716@hr.nl>
2016-03-22 13:15:18 +0100

    Shizzles met GUI
    Can you see him? NO! IT'S JOHN CENA!

---
+++ dev/pinautomaat/Interface/GuiTest/GuiTest/GeldOpnemen.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace GuiTest
+{
+    partial class GeldOpnemen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnVorige = new System.Windows.Forms.Button();
+            this.btnUitloggen = new System.Windows.Forms.Button();
+            this.btnPrintBonNiet = new System.Windows.Forms.Button();
+            this.btnPrintBonWel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputGeldOpnemen = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 11;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.091001F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.091001F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.091001F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.091001F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.091001F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.091001F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.091001F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.091001F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.091001F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.090091F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.090909F));
+            this.tableLayoutPanel1.Controls.Add(this.btnVorige, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.btnUitloggen, 1, 9);
+            this.tableLayoutPanel1.Controls.Add(this.btnPrintBonNiet, 7, 9);
+            this.tableLayoutPanel1.Controls.Add(this.btnPrintBonWel, 7, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.inputGeldOpnemen, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 12;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.333417F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.333417F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.333417F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.333417F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.333417F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.333417F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.333417F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.333417F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.333417F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.332583F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.333333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.333333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1000, 750);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // btnVorige
+            // 
+            this.btnVorige.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tableLayoutPanel1.SetColumnSpan(this.btnVorige, 3);
+            this.btnVorige.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVorige.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnVorige.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnVorige.FlatAppearance.BorderSize = 0;
+            this.btnVorige.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVorige.Font = new System.Drawing.Font("Open Sans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVorige.ForeColor = System.Drawing.Color.White;
+            this.btnVorige.Location = new System.Drawing.Point(90, 372);
+            this.btnVorige.Margin = new System.Windows.Forms.Padding(0);
+            this.btnVorige.Name = "btnVorige";
+            this.tableLayoutPanel1.SetRowSpan(this.btnVorige, 2);
+            this.btnVorige.Size = new System.Drawing.Size(270, 124);
+            this.btnVorige.TabIndex = 14;
+            this.btnVorige.Text = "[A] Vorige";
+            this.btnVorige.UseVisualStyleBackColor = false;
+            this.btnVorige.Click += new System.EventHandler(this.btnVorige_Click);
+            // 
+            // btnUitloggen
+            // 
+            this.btnUitloggen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tableLayoutPanel1.SetColumnSpan(this.btnUitloggen, 3);
+            this.btnUitloggen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUitloggen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnUitloggen.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnUitloggen.FlatAppearance.BorderSize = 0;
+            this.btnUitloggen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUitloggen.Font = new System.Drawing.Font("Open Sans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUitloggen.ForeColor = System.Drawing.Color.White;
+            this.btnUitloggen.Location = new System.Drawing.Point(90, 558);
+            this.btnUitloggen.Margin = new System.Windows.Forms.Padding(0);
+            this.btnUitloggen.Name = "btnUitloggen";
+            this.tableLayoutPanel1.SetRowSpan(this.btnUitloggen, 2);
+            this.btnUitloggen.Size = new System.Drawing.Size(270, 124);
+            this.btnUitloggen.TabIndex = 11;
+            this.btnUitloggen.Text = "[C] Uitloggen";
+            this.btnUitloggen.UseVisualStyleBackColor = false;
+            this.btnUitloggen.Click += new System.EventHandler(this.btnUitloggen_Click);
+            // 
+            // btnPrintBonNiet
+            // 
+            this.btnPrintBonNiet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tableLayoutPanel1.SetColumnSpan(this.btnPrintBonNiet, 3);
+            this.btnPrintBonNiet.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPrintBonNiet.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnPrintBonNiet.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnPrintBonNiet.FlatAppearance.BorderSize = 0;
+            this.btnPrintBonNiet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrintBonNiet.Font = new System.Drawing.Font("Open Sans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrintBonNiet.ForeColor = System.Drawing.Color.White;
+            this.btnPrintBonNiet.Location = new System.Drawing.Point(630, 558);
+            this.btnPrintBonNiet.Margin = new System.Windows.Forms.Padding(0);
+            this.btnPrintBonNiet.Name = "btnPrintBonNiet";
+            this.tableLayoutPanel1.SetRowSpan(this.btnPrintBonNiet, 2);
+            this.btnPrintBonNiet.Size = new System.Drawing.Size(270, 124);
+            this.btnPrintBonNiet.TabIndex = 10;
+            this.btnPrintBonNiet.Text = "[D] Ga verder en print bon niet uit";
+            this.btnPrintBonNiet.UseVisualStyleBackColor = false;
+            // 
+            // btnPrintBonWel
+            // 
+            this.btnPrintBonWel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tableLayoutPanel1.SetColumnSpan(this.btnPrintBonWel, 3);
+            this.btnPrintBonWel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPrintBonWel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnPrintBonWel.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnPrintBonWel.FlatAppearance.BorderSize = 0;
+            this.btnPrintBonWel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrintBonWel.Font = new System.Drawing.Font("Open Sans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrintBonWel.ForeColor = System.Drawing.Color.White;
+            this.btnPrintBonWel.Location = new System.Drawing.Point(630, 372);
+            this.btnPrintBonWel.Margin = new System.Windows.Forms.Padding(0);
+            this.btnPrintBonWel.Name = "btnPrintBonWel";
+            this.tableLayoutPanel1.SetRowSpan(this.btnPrintBonWel, 2);
+            this.btnPrintBonWel.Size = new System.Drawing.Size(270, 124);
+            this.btnPrintBonWel.TabIndex = 9;
+            this.btnPrintBonWel.Text = "[B] Ga verder en print bon wel uit";
+            this.btnPrintBonWel.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.tableLayoutPanel1.SetColumnSpan(this.label1, 11);
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.tableLayoutPanel1.SetRowSpan(this.label1, 2);
+            this.label1.Size = new System.Drawing.Size(1000, 124);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hoeveel geld wilt u opnemen?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inputGeldOpnemen
+            // 
+            this.inputGeldOpnemen.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tableLayoutPanel1.SetColumnSpan(this.inputGeldOpnemen, 9);
+            this.inputGeldOpnemen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputGeldOpnemen.Font = new System.Drawing.Font("Microsoft Sans Serif", 82F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputGeldOpnemen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.inputGeldOpnemen.Location = new System.Drawing.Point(90, 186);
+            this.inputGeldOpnemen.Margin = new System.Windows.Forms.Padding(0);
+            this.inputGeldOpnemen.Name = "inputGeldOpnemen";
+            this.inputGeldOpnemen.Size = new System.Drawing.Size(810, 124);
+            this.inputGeldOpnemen.TabIndex = 8;
+            this.inputGeldOpnemen.Text = "100";
+            this.inputGeldOpnemen.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // GeldOpnemen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(1000, 750);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "GeldOpnemen";
+            this.Text = "Form3";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputGeldOpnemen;
+        private System.Windows.Forms.Button btnPrintBonNiet;
+        private System.Windows.Forms.Button btnPrintBonWel;
+        private System.Windows.Forms.Button btnUitloggen;
+        private System.Windows.Forms.Button btnVorige;
+    }
+}
---
+++ dev/pinautomaat/Interface/GuiTest/GuiTest/MainMenu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuiTest
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnUitloggen_Click(object sender, EventArgs e)
+        {
+            new Welkom().Show();
+            Thread.Sleep(10); //GEVAARLIJK!!!!
+            this.Hide();
+        }
+
+        private void btnSaldo_Click(object sender, EventArgs e)
+        {
+            new Saldo().Show();
+            Thread.Sleep(10); //GEVAARLIJK!!!!
+            this.Hide();
+        }
+
+        private void btnGeldOpnemen_Click(object sender, EventArgs e)
+        {
+            new GeldOpnemen().Show();
+            Thread.Sleep(10); //GEVAARLIJK!!!!
+            this.Hide();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
