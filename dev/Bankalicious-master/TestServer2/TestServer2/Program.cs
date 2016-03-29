774b211dfd089748f91a7ecdd5462d73a9f86fe1
Remco La Brijn <0894807@hr.nl>
2016-03-29 16:20:18 +0200

    still not workin
    WAAAAAAAAAAAAAAAAAAAAAAAAAH

--- dev/Bankalicious-master/TestServer2/TestServer2/Program.cs
+++ dev/Bankalicious-master/TestServer2/TestServer2/Program.cs
@@ -14,45 +14,20 @@ namespace TestServer2
             //we maken een nieuwe database-context. Als de database nog niet bestaat; wordt deze aangemaakt
             using (var db = new BankContext())
             {
-
-                Console.WriteLine("Voer de naam van de eerste klant in:");
-                string name = Console.ReadLine();
-
-                //we maken een nieuw klant-object aan
-                var Klant = new Klant { Naam = name };
-
-                //we we voegen deze klant aan de database toe
-                db.Klant.Add(Klant);
-
                 //en we slaan de nieuwe dingen in de database op
                 db.SaveChanges();
-
-                Console.WriteLine("\nDe klanten zijn:");
-
-                //we lopen door alle klanten heen. Op de achtergrond wordt er sql uitgevoerd.
-                //db.clients is een property, net als de properties die je onderaan hebt aangemaakt
-                foreach (var item in db.Klant)
-                {
-                    Console.WriteLine(item.Name);
-                }
-                Console.WriteLine();
-                Console.WriteLine("please enter the name of your new bank account");
-                var account = Console.ReadLine();
-                var Klant = new Klant { KlantID = KlantID, Title = account};
-                db.Klant.Add(Account);
-                db.SaveChanges();
                 Console.WriteLine("\nDe rekeningen zijn:");
-                foreach (var item in db.Klant)
+                foreach (var item in )
                 {
-                    Console.WriteLine("rekeningnaam: " + item.Title);
-                    Console.WriteLine("eigenaar: " + item.Klant.Name);
+                    Console.WriteLine("rekeningnaam: " + item.Rekening.RekeningType);
+                    Console.WriteLine("eigenaar: " + item.Klant.Naam);
                     Console.WriteLine();
                 }
 
                 Console.WriteLine("Voer de naam in van de klant die je wilt zoeken");
                 var zoekstring = Console.ReadLine();
-                var GevondenKlant = db.Klant.Where(x => x.Name.Contains(zoekstring)).First();
-                Console.Write(GevondenKlant.Name);
+                var GevondenKlant = db.Klant.Where(x => x.Naam.Contains(zoekstring)).First();
+                Console.Write(GevondenKlant.Naam);
                 Console.ReadKey();
             }
                 
@@ -67,6 +42,9 @@ namespace TestServer2
         public BankContext() : base("MyDbContextConnectionString") { }
         public DbSet<Klant> Klant { get; set; }
         public DbSet<Rekening> Rekening { get; set; }
+        public DbSet<Pas> Pas { get; set; }
+        public DbSet<Transactie> Transactie { get; set; }
+
     }
 
     /// <summary>
